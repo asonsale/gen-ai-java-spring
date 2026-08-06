66bf07814af95f145e4f5b21bbd857ce4e45f183
Anand Sonsale <asonsale@gmail.com>
2026-08-06 11:49:20 +0530

    initial commit

---
+++ src/test/java/com/geni/java/spring/GenAiJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geni.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenAiJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
