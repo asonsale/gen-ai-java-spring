11e036890cf9a5663530162648b6889c4565fcc8
Anand Sonsale <asonsale@gmail.com>
2026-08-07 11:38:40 +0530

    Create custom class exception

---
+++ src/main/java/com/geni/java/spring/chat/openai/OpenAIChatException.java
@@ -0,0 +1,12 @@
+package com.geni.java.spring.chat.openai;
+
+public class OpenAIChatException extends RuntimeException {
+
+    public OpenAIChatException(String message) {
+        super(message);
+    }
+
+    public OpenAIChatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
